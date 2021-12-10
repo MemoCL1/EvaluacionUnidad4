79e820b3a0b5685348d6fed64eca9601f74349f8
Guillermo Abel <angrilek@gmail.com>
2021-12-10 02:17:56 -0600

    Inicio

--- ClassesEvaluacion4.py
+++ ClassesEvaluacion4.py
@@ -1,2 +1,12 @@
 "Este programa muestra una simulacion de una oficina"
 
+class persona:
+
+    def __init__(self,nombre,apellidos,edad,altura,peso,saldoCuenta,fechaNacimiento):
+        self.__nombre = nombre
+        self.__apellidos = apellidos
+        self.__edad = edad
+        self.__altura = altura
+        self.__peso = peso
+        self.__credito = float(saldoCuenta)
+        self.__nacimiento = fechaNacimiento
