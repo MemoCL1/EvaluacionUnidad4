2d2ea26e62d0abec1168280dd2d8c54658911bc1
Guillermo Abel <angrilek@gmail.com>
2021-12-10 02:43:02 -0600

    clases definidas

--- ClassesEvaluacion4.py
+++ ClassesEvaluacion4.py
@@ -1,4 +1,4 @@
-"Este programa muestra una simulacion de una oficina"
+"Aqui van todoas las clases que se usaron y varios metodos"
 
 class persona:
 
@@ -10,3 +10,32 @@ class persona:
         self.__peso = peso
         self.__credito = float(saldoCuenta)
         self.__nacimiento = fechaNacimiento
+
+    def saludar(self):
+        print("Hola buenos dias")
+
+    def presentarse(self):
+        print('Mi nombre es ', self.nombre)
+
+    def presentarseFormalmente(self):
+        print("Mi nombre completo es",self.__nombre,self.__apellidos)
+
+class Empleado(persona):
+    def __init__(self, nombre, altura, vestimenta, empresa, puesto):
+        super().__init__(nombre, altura, vestimenta)
+        self.empresa = empresa
+        self.puesto = puesto
+
+    def saludar(self):
+        print(f'Hola me llamo {self.nombre} y soy empleado de {self.empresa}')
+
+class Gerente(Empleado):
+    def __init__(self, nombre, altura, vestimenta, empresa, puesto):
+        super().__init__(nombre, altura, vestimenta, empresa, puesto)
+        self.puesto = 'Gerente'
+
+    def saludar(self):
+        print(f'Soy el gerente de {self.empresa}, mi nombre es {self.nombre}')
+
+    def entrevistar(self, persona):
+        print(f'Estoy entrevistando a {persona}')
--- mainEvalucacion4.py
+++ mainEvalucacion4.py
@@ -1,1 +1,2 @@
-
+import ClassesEvaluacion4 as Entidad
+empleado1=Entidad.persona("Pepe","Macias",26,1.85,80,1500,"3 DE AGOSTO")
